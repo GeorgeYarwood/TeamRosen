1d221f60d7c8a3765c49ed4c8e8f7b6da8244596
George <george.yarwood@gmail.com>
2021-09-15 02:30:14 +0100

    Basic character controller working

--- Team Rosen/Assets/Scripts/Player/PlayerController.cs
+++ Team Rosen/Assets/Scripts/Player/PlayerController.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Player's rigidbody
     Rigidbody playerRB;
 
-    
-
+    //Speed player moves at
     float moveSpeed = 20f;
-    float strafeSpeed = 5f;
 
+    //Force applied when jumping
+    float jumpSpeed = 400f;
+
+    //Jump cooldown timer
+    float jumpCooldown = 1f;
+
+    //Up vector
     Vector3 upVec = new Vector3(0, 1, 0);
 
+    //Overwritten with values
     float rotationX = 0;
-
+    float rotationY = 0;
 
     //Camera sensitivity
     float sens = 3f;
 
+    //Last mosue position
     Vector3 lastMouse;
+
+
+    //Cooldown for jump
+    bool canJump;
 
     // Start is called before the first frame update
     void Start()
@@ -27,13 +39,14 @@ public class PlayerController : MonoBehaviour
         //Get player rigidbody
         playerRB = GetComponent<Rigidbody>();
 
+        //Save last mouse pos
         lastMouse = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0) - lastMouse;
 
 
         //Hide and lock cursor
         Cursor.lockState = CursorLockMode.Locked;
 
-
+        canJump = true;
     }
 
     void FixedUpdate() 
@@ -42,7 +55,7 @@ public class PlayerController : MonoBehaviour
         ///Keyboard movements
         ///
             //Forward/Backward
-            if (Input.GetKey("w")) 
+            if (Input.GetKey("w"))
             {
                 playerRB.AddForce(playerRB.transform.forward * moveSpeed);
             }
@@ -56,27 +69,51 @@ public class PlayerController : MonoBehaviour
 
             if (Input.GetKey("a"))
             {
-                //Get right vector
-                playerRB.AddForce(Vector3.Cross(playerRB.transform.forward, upVec.normalized)* moveSpeed);
+                playerRB.AddForce(Vector3.Cross(playerRB.transform.forward, upVec.normalized) * moveSpeed);
+
             }
             if (Input.GetKey("d"))
             {
+
+                //Get right vector
                 playerRB.AddForce(-(Vector3.Cross(playerRB.transform.forward, upVec.normalized) * moveSpeed));
-        }
+            }
+
+            //Jump
+            if (Input.GetKey("space")) 
+            {
+                //If jump off cooldown
+                if (canJump) 
+                {
+                    playerRB.AddForce(playerRB.transform.up * jumpSpeed);
+                    StartCoroutine(jumpTimer());
+                }
+                
+            }
 
         ///Mouse movements
         ///
 
 
+            //Get mouse movements
+            float horizontal = sens * Input.GetAxis("Mouse X");
+            float vertical = sens * Input.GetAxis("Mouse Y");
 
-        float horizontal = sens * Input.GetAxis("Mouse X");
-        float vertical = sens * Input.GetAxis("Mouse Y");
 
-        rotationX += Input.GetAxis("Mouse Y") * sens;
+            //Assign and invert Y axis
+            rotationX += Input.GetAxis("Mouse Y") * -sens;
+            rotationY += Input.GetAxis("Mouse X") * sens;
 
-        playerRB.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-        //playerRB.transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * sens, 0);
+            //Apply rotation to rigidbody
+            playerRB.transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0);
 
+    }
+
+    IEnumerator jumpTimer() 
+    {
+        canJump = false;
+        yield return new WaitForSeconds(jumpCooldown);
+        canJump = true;
     }
 
     // Update is called once per frame
