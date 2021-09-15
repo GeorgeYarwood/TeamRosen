09fa66feda75f95b8c1643ae18e53de311c6812f
Lucas Soares <minikingplays@gmail.com>
2021-09-15 01:20:22 +0100

    Further advances on Trashcan Spawn
    .

--- Team Rosen/Assets/Scripts/LootSpawners/TrashCanSpawn.cs
+++ Team Rosen/Assets/Scripts/LootSpawners/TrashCanSpawn.cs
@@ -5,24 +5,32 @@ using UnityEngine;
 public class TrashCanSpawn : MonoBehaviour
 {
     bool isCollected;
-    public GameObject TrashCan;
-    public float SpawnRate = 1f;
-    public float SpawnChance = 1f;
-
+    public GameObject[] TrashCans;
+    
     public 
     // Start is called before the first frame update
     void Start()
     {
-        TrashCan.SetActive(false);
+        /*TrashCan.SetActive(false);
         if (Random.value < SpawnChance)
         {
             TrashCan.SetActive(true);
+        }*/
+        
+        for (int i=0; i<TrashCans.Length; i++)
+        {
+            TrashCans[i].SetActive(false);
         }
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        int randomIndex = Random.Range(0, TrashCans.Length);
+        TrashCans[randomIndex].SetActive(true);
+        
         
     }
 }
