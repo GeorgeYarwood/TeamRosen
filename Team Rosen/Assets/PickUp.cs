535054297a15eb25bf99d47fa786e93719100b17
GeorgeYarwood <george.yarwood@gmail.com>
2021-09-16 14:55:41 +0100

    Fixed pick up script

--- Team Rosen/Assets/PickUp.cs
+++ Team Rosen/Assets/PickUp.cs
@@ -12,13 +12,55 @@ public class PickUp : MonoBehaviour
     public GameObject tempParent;
     public bool isHolding = false;
 
+
+
+ 
     void Update()
     {
-        if(isHolding==true)
+        //If player left clicks
+        if (Input.GetMouseButton(0)) 
         {
-            item.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+            //Check is player is looking at a bag
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            //If we hit
+            if (Physics.Raycast(ray, out hit, 10f))
+            {
+                if (hit.transform.tag == "Bag")
+                {
+                    //Set the current item to the gameobject we hit
+                    item = hit.transform.gameObject;
+
+                    isHolding = true;
+
+                    item.GetComponent<Rigidbody>().useGravity = false;
+                    item.GetComponent<Rigidbody>().detectCollisions = true;
+                }
+
+            }
+
+            
+        }
+
+        //If player releases left click
+        if (Input.GetMouseButtonUp(0)) 
+        {
+            isHolding = false;
+
+        }
+
+
+        if (isHolding==true)
+        {
+            //item.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            //item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            item.transform.GetComponent<BoxCollider>().enabled = false;
             item.transform.SetParent(tempParent.transform);
+
+            //Vector3 modPos = new Vector3(tempParent.transform.position.x +2, tempParent.transform.position.y, tempParent.transform.position.z);
+            item.transform.position = tempParent.transform.position;
             
             if(Input.GetMouseButtonDown(1))
             {
@@ -26,6 +68,8 @@ public class PickUp : MonoBehaviour
             }
             else
             {
+                item.transform.GetComponent<BoxCollider>().enabled = true;
+
                 objectPos = item.transform.position;
                 item.transform.SetParent(null);
                 item.GetComponent<Rigidbody>().useGravity = true;
