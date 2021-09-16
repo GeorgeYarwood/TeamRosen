0c9cf53390913b229a05080c084be9929ff85014
Max Pettigrew <pettigrewm@coventry.ac.uk>
2021-09-16 10:29:03 +0100

    Music system

---
+++ Team Rosen/Assets/Scripts/music system.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicsystem : MonoBehaviour
+{ static AudioSource musicPlayer;
+ static AudioClip [] clips = new AudioClip[3];
+ Object [] tracks;   
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicPlayer = gameObject.AddComponent<AudioSource>();
+
+        LoadTracks();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadTracks()
+    {
+        //find all audio clips in the projject
+
+        tracks = Resources.LoadAll("music",typeof(AudioClip));
+        FillArray(tracks);
+    } 
+
+    void FillArray(Object[] tracklist)
+    {
+       for(int i = 0 ; i <= clips.Length; i++)
+       { 
+          try
+          {
+              clips[i] = (AudioClip)tracklist[i];
+          }
+          catch
+          {
+        
+    
+          } 
+       }
+
+    }
+}
